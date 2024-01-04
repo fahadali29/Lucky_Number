5457f60fe3a602f75cc5ee9b7c4850ceb6bccb38
fhd <fahadali2973747@gmail.com>
2024-01-04 03:10:07 -0800

    ShareResult Functionality added

--- app/src/main/java/com/example/luckynumber/Activity2.java
+++ app/src/main/java/com/example/luckynumber/Activity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,7 +22,14 @@ public class Activity2 extends AppCompatActivity {
         share = findViewById(R.id.button);
         Intent intent = getIntent();
         String input = intent.getStringExtra("Name");
-        rendom.setText("" + rendom());
+        int result = rendom();
+        rendom.setText("" + result);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareResult(input , result);
+            }
+        });
 
     }
     public int rendom(){
@@ -29,5 +37,12 @@ public class Activity2 extends AppCompatActivity {
         int uperrlimit = 1000;
         int rendomgenerte = random.nextInt(uperrlimit);
         return rendomgenerte;
+    }
+    public void ShareResult(String input , int result ){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT , input + " got lucky today" );
+        intent.putExtra(Intent.EXTRA_TEXT , "His lucky number is " + result);
+        startActivity(Intent.createChooser(intent , "Choose a platform"));
     }
 }
